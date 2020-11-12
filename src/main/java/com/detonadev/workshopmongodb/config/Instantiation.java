1c2bb7ae902de29a39ce8e33173dd37cacd80ae6
tonpasso <wellpasso@gmail.com>
2020-11-12 16:43:51 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/detonadev/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/detonadev/workshopmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.detonadev.workshopmongodb.domain.Post;
 import com.detonadev.workshopmongodb.domain.User;
+import com.detonadev.workshopmongodb.dto.AuthorDTO;
 import com.detonadev.workshopmongodb.repository.PostRepository;
 import com.detonadev.workshopmongodb.repository.UserRepository;
 
@@ -37,8 +38,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, date.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, date.parse("23/05/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, date.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, date.parse("23/05/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
